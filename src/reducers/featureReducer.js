53d2bf614f92ab8b9f5eb8298caf73ee324fc814
garybot <argot.pryer@gmail.com>
2019-11-21 18:03:16 -0800

    fixed bug involving REMOVE_FEATURE in the reducer."

--- src/reducers/featureReducer.js
+++ src/reducers/featureReducer.js
@@ -28,7 +28,7 @@ export const featureReducer = (state=initialState, action) => {
       return {
         ...state,
         additionalPrice: state.additionalPrice - action.payload.price,
-        car: {...state.car, features: [state.car.features.filter(feature => feature.id !== action.payload.id)]}
+        car: {...state.car, features: state.car.features.filter(feature => feature.id !== action.payload.id)}
       }
     default:
       return state;
